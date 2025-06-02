f233259a1c150a819dfa9012d78ecbe523b1fd45
KhoirulArifPratama <khoirularif932@gmail.com>
2025-06-02 11:20:21 +0700

    Add baseClass and derivedClass with method overriding demonstration

--- abstraksi/noOverriding/noPewarisan/noPewarisan.cpp
+++ abstraksi/noOverriding/noPewarisan/noPewarisan.cpp
@@ -9,3 +9,21 @@ public:
         cout << "Hello saya Function dari base class";
     }
 };
+
+class derivedClass : public baseClass
+{
+public:
+    void perkenalan()
+    {
+        cout << "Hello saya Function dari derived Class";
+    }
+};
+
+int main()
+{
+    derivedClass a;
+
+    a.perkenalan();
+
+    return 0;
+}
