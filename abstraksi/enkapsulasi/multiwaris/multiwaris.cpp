14cdef6d4d296c9f6a06c2501edf4503be18a8d3
KhoirulArifPratama <khoirularif932@gmail.com>
2025-06-02 11:12:27 +0700

    Implement budi class using multiple inheritance from pekerja and pelajar

--- abstraksi/enkapsulasi/multiwaris/multiwaris.cpp
+++ abstraksi/enkapsulasi/multiwaris/multiwaris.cpp
@@ -33,3 +33,22 @@ public:
              << endl;
     }
 };
+
+class budi : public pekerja, public pelajar
+{
+public:
+    budi(int pUmur) : pekerja(pUmur),
+                      pelajar(pUmur),
+                      orang(pUmur)
+    {
+        cout << "Budi dibuat \n"
+             << endl;
+    }
+};
+
+int main()
+{
+    budi a(12);
+
+    return 0;
+}
